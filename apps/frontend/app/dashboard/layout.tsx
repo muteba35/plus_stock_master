eb71857250deb44cd3cdbf9b1e8445abd0600612
muteba <juniormuteba10@gmail.com>
2026-06-06 00:17:14 +0100

    Add Netlif

--- apps/frontend/app/dashboard/layout.tsx
+++ apps/frontend/app/dashboard/layout.tsx
@@ -250,7 +250,7 @@ export default function DashboardLayout({
     { 
       name: "Employés", 
       href: "/dashboard/equipe/employes", 
-      permission: "VOIR_EMPLOYES" //
+      permission: "VOIR_EMPLOYE" //
     },
     { 
       name: "Départements", 
